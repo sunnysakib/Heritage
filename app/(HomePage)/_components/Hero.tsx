d44fd047c84428a5b255622f5647984fc229c108
sunnysakib <sakibur.rahaman033@gmail.com>
2024-08-12 12:02:12 +0600

    feat: create hero section

---
+++ app/(HomePage)/_components/Hero.tsx
@@ -0,0 +1,22 @@
+import CustomButton from "@/components/CustomButton";
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-[url('/assets/images/hero.png')] bg-cover bg-center h-[500px]">
+      <div className="flex flex-col gap-y-4 items-center justify-center h-full">
+        <h1 className="w-[696px] text-center text-6xl font-bold text-white">
+          Your Portal to India&apos;s Exquisite Real Estate
+        </h1>
+        <p className="w-[696px] text-center text-[16px]  text-white">
+          Seamlessly connecting you to the heartbeat of India&apos;s prime
+          properties.
+        </p>
+        <CustomButton iconsBg="text-white" bg="bg-blue-500 hover:bg-blue-600" label="Find Property" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        {children}</body>
+        {children}
+        <Footer/>
+        </body>
     </html>
   );
 }
